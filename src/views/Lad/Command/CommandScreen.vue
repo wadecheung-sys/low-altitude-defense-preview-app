885584e902df1595b2d5618cd7f4e6d72e3bae67
wadecheung-sys <wade.cheung@icloud.com>
2026-08-20 14:25:40 +0800

    feat: refresh data screen prototype

--- src/views/Lad/Command/CommandScreen.vue
+++ src/views/Lad/Command/CommandScreen.vue
@@ -10,7 +10,7 @@ const PROTOTYPE_WIDTH = 1920
 const PROTOTYPE_HEIGHT = 1080
 const DISPLAY_MAX_WIDTH = 1920
 const PROTOTYPE_PAGE = encodeURIComponent('数据大屏03.html')
-const PROTOTYPE_VERSION = '20260819-native-device-detail-v2'
+const PROTOTYPE_VERSION = '20260820-prototype-update'
 const PROTOTYPE_SRC = `${import.meta.env.BASE_URL}prototypes/data-screen-03/${PROTOTYPE_PAGE}?v=${PROTOTYPE_VERSION}`
 
 type Cleanup = () => void
