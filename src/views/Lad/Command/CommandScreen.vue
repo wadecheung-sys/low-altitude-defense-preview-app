9ad1da921513d569c45c2c4298c2676bb3501bd3
wadecheung-sys <wade.cheung@icloud.com>
2026-09-07 19:47:00 +0800

    feat: publish latest versioned data screen

--- src/views/Lad/Command/CommandScreen.vue
+++ src/views/Lad/Command/CommandScreen.vue
@@ -8,9 +8,10 @@ defineOptions({ name: 'LadDataScreen' })
 const PROTOTYPE_WIDTH = 1920
 const PROTOTYPE_HEIGHT = 1080
 const DISPLAY_MAX_WIDTH = 1920
+const PROTOTYPE_DIRECTORY = 'data-screen-03-20260907-193922'
 const PROTOTYPE_PAGE = encodeURIComponent('数据大屏03.html')
-const PROTOTYPE_VERSION = '20260907-evening-prototype-export'
-const PROTOTYPE_SRC = `${import.meta.env.BASE_URL}prototypes/data-screen-03/${PROTOTYPE_PAGE}?v=${PROTOTYPE_VERSION}`
+const PROTOTYPE_VERSION = '20260907-193922'
+const PROTOTYPE_SRC = `${import.meta.env.BASE_URL}prototypes/${PROTOTYPE_DIRECTORY}/${PROTOTYPE_PAGE}?v=${PROTOTYPE_VERSION}`
 
 type Cleanup = () => void
 type IdleCapableWindow = Window & {
