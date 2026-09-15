77331aa7551e70d8b17da9fb87d18e79dc79fb2c
wadecheung-sys <wade.cheung@icloud.com>
2026-09-15 11:35:50 +0800

    feat: publish latest September 15 data screen

--- src/views/Lad/Command/CommandScreen.vue
+++ src/views/Lad/Command/CommandScreen.vue
@@ -8,9 +8,9 @@ defineOptions({ name: 'LadDataScreen' })
 const PROTOTYPE_WIDTH = 1920
 const PROTOTYPE_HEIGHT = 1080
 const DISPLAY_MAX_WIDTH = 1920
-const PROTOTYPE_DIRECTORY = 'data-screen-03-20260915-091801'
+const PROTOTYPE_DIRECTORY = 'data-screen-03-20260915-113208'
 const PROTOTYPE_PAGE = encodeURIComponent('数据大屏03.html')
-const PROTOTYPE_VERSION = '20260915-091801'
+const PROTOTYPE_VERSION = '20260915-113208'
 const PROTOTYPE_SRC = `${import.meta.env.BASE_URL}prototypes/${PROTOTYPE_DIRECTORY}/${PROTOTYPE_PAGE}?v=${PROTOTYPE_VERSION}`
 
 type Cleanup = () => void
